e8302a946655ffaeec1d7bcd51331cdc39deabe1
Robin Cloutman <rycochet@rycochet.com>
2011-09-25 12:34:29 +0100

    Change groupId.

--- src/main/java/mmo/Info/MMOInfo.java
+++ src/main/java/mmo/Info/MMOInfo.java
@@ -16,7 +16,6 @@
  */
 package mmo.Info;
 
-import mmo.Core.events.MMOInfoEvent;
 import java.util.Arrays;
 import java.util.BitSet;
 import java.util.regex.Matcher;
@@ -24,11 +23,11 @@ import java.util.regex.Pattern;
 import mmo.Core.MMO;
 import mmo.Core.MMOListener;
 import mmo.Core.MMOPlugin;
-import mmo.Core.events.MMOHUDEvent;
+import mmo.Core.MMOHUDEvent;
+import mmo.InfoAPI.MMOInfoEvent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.Plugin;
@@ -43,7 +42,6 @@ import org.getspout.spoutapi.gui.GenericLabel;
 import org.getspout.spoutapi.gui.GenericTexture;
 import org.getspout.spoutapi.gui.RenderPriority;
 import org.getspout.spoutapi.gui.Screen;
-import org.getspout.spoutapi.gui.Texture;
 import org.getspout.spoutapi.gui.Widget;
 import org.getspout.spoutapi.gui.WidgetAnchor;
 import org.getspout.spoutapi.player.SpoutPlayer;
@@ -62,39 +60,32 @@ public class MMOInfo extends MMOPlugin {
 	@Override
 	public void onEnable() {
 		super.onEnable();
-		MMO.mmoInfo = true;
 		pm.registerEvent(Type.CUSTOM_EVENT,
-				  new MMOListener() {
+				new MMOListener() {
 
-					  @Override
-					  public void onMMOInfo(MMOInfoEvent event) {
-						  if (event.isToken("name")) {
-							  Player player = event.getPlayer();
-							  event.setWidget(plugin, new GenericLabel(MMO.getName(player)).setResize(true).setFixed(true));
-						  }
-					  }
+					@Override
+					public void onMMOInfo(MMOInfoEvent event) {
+						if (event.isToken("name")) {
+							Player player = event.getPlayer();
+							event.setWidget(plugin, new GenericLabel(MMO.getName(player)).setResize(true).setFixed(true));
+						}
+					}
 
-					  @Override
-					  public void onMMOHUD(MMOHUDEvent event) {
-						  Player player = event.getPlayer();
-						  if (player.hasPermission("mmo.info.display")) {
-							  if (event.getAnchor() == WidgetAnchor.TOP_LEFT || event.getAnchor() == WidgetAnchor.TOP_CENTER || event.getAnchor() == WidgetAnchor.TOP_RIGHT) {
-								  event.setOffsetY(event.getOffsetY() + height + offset + 1);
-							  }
-						  }
-					  }
-				  }, Priority.Normal, this);
+					@Override
+					public void onMMOHUD(MMOHUDEvent event) {
+						Player player = event.getPlayer();
+						if (player.hasPermission("mmo.info.display")) {
+							if (event.getAnchor() == WidgetAnchor.TOP_LEFT || event.getAnchor() == WidgetAnchor.TOP_CENTER || event.getAnchor() == WidgetAnchor.TOP_RIGHT) {
+								event.setOffsetY(event.getOffsetY() + height + offset + 1);
+							}
+						}
+					}
+				}, Priority.Normal, this);
 	}
 
 	@Override
 	public void loadConfiguration(Configuration cfg) {
 		config_info = cfg.getString("info", config_info);
-	}
-
-	@Override
-	public void onDisable() {
-		MMO.mmoInfo = false;
-		super.onDisable();
 	}
 
 	@Override
@@ -120,9 +111,9 @@ public class MMOInfo extends MMOPlugin {
 		screen.attachWidget(plugin, new GenericGradient().setBottomColor(back).setTopColor(back).setMaxWidth(2048).setWidth(2048).setHeight(height + offset).setAnchor(WidgetAnchor.TOP_LEFT).setPriority(RenderPriority.Highest));
 		screen.attachWidget(plugin, new GenericGradient().setBottomColor(bottom).setTopColor(bottom).setY(height + offset).setMaxWidth(2048).setWidth(2048).setHeight(1).setAnchor(WidgetAnchor.TOP_LEFT).setPriority(RenderPriority.Highest));
 		screen.attachWidget(plugin, new GenericContainer(
-				  current = left = (Container) new GenericContainer().setLayout(ContainerType.HORIZONTAL).setAlign(WidgetAnchor.TOP_LEFT).setAnchor(WidgetAnchor.TOP_LEFT).setWidth(427).setHeight(height).setY(offset),
-				  center = (Container) new GenericContainer().setLayout(ContainerType.HORIZONTAL).setAlign(WidgetAnchor.TOP_CENTER).setAnchor(WidgetAnchor.TOP_CENTER).setWidth(427).setHeight(height).setX(-213).setY(offset),
-				  right = (Container) new GenericContainer().setLayout(ContainerType.HORIZONTAL).setAlign(WidgetAnchor.TOP_RIGHT).setAnchor(WidgetAnchor.TOP_RIGHT).setWidth(427).setHeight(height).setX(-427).setY(offset)));
+				current = left = (Container) new GenericContainer().setLayout(ContainerType.HORIZONTAL).setAlign(WidgetAnchor.TOP_LEFT).setAnchor(WidgetAnchor.TOP_LEFT).setWidth(427).setHeight(height).setY(offset),
+				center = (Container) new GenericContainer().setLayout(ContainerType.HORIZONTAL).setAlign(WidgetAnchor.TOP_CENTER).setAnchor(WidgetAnchor.TOP_CENTER).setWidth(427).setHeight(height).setX(-213).setY(offset),
+				right = (Container) new GenericContainer().setLayout(ContainerType.HORIZONTAL).setAlign(WidgetAnchor.TOP_RIGHT).setAnchor(WidgetAnchor.TOP_RIGHT).setWidth(427).setHeight(height).setX(-427).setY(offset)));
 
 		Matcher match = Pattern.compile("[^{~]+|(~)|\\{([^}]*)\\}").matcher(config_info);
 		while (match.find()) {
@@ -136,16 +127,18 @@ public class MMOInfo extends MMOPlugin {
 				}
 			} else if (match.group(2) != null) {
 				String[] args = MMO.smartSplit(match.group(2));
-				MMOInfoEventEvent event = new MMOInfoEventEvent(player, args[0], Arrays.copyOfRange(args, 1, args.length));
+				MMOInfoEvent event = new MMOInfoEvent(player, args[0], Arrays.copyOfRange(args, 1, args.length));
 				pm.callEvent(event);
-				if (!event.isCancelled() && event.widget != null) {
-					if (event.icon != null) {
-						Widget icon = new GenericTexture(event.icon).setMargin(0, 0, 0, 3).setHeight(8).setWidth(8).setFixed(true);
-						screen.attachWidget(event.plugin, icon);
+				if (!event.isCancelled() && event.getWidget() != null) {
+					Widget widget = event.getWidget();
+					Plugin widgetPlugin = event.getPlugin();
+					if (event.getIcon() != null) {
+						Widget icon = new GenericTexture(event.getIcon()).setMargin(0, 0, 0, 3).setHeight(8).setWidth(8).setFixed(true);
+						screen.attachWidget(widgetPlugin, icon);
 						current.addChild(icon);
 					}
-					screen.attachWidget(event.plugin, event.widget.setMargin(0, 3));
-					current.addChild(event.widget);
+					screen.attachWidget(widgetPlugin, widget.setMargin(0, 3));
+					current.addChild(widget);
 				}
 			} else {
 				String str = match.group().trim();
@@ -153,65 +146,6 @@ public class MMOInfo extends MMOPlugin {
 					current.addChild(new GenericLabel(str).setResize(true).setMargin(0, 3).setFixed(true));
 				}
 			}
-		}
-	}
-
-	private class MMOInfoEventEvent extends Event implements MMOInfoEvent {
-
-		SpoutPlayer player;
-		String token;
-		String[] args;
-		Plugin plugin = null;
-		Widget widget = null;
-		String icon = null;
-		boolean cancelled = false;
-
-		public MMOInfoEventEvent(SpoutPlayer player, String token, String[] args) {
-			super("mmoInfoEvent");
-			this.player = player;
-			this.token = token;
-			this.args = args;
-		}
-
-		@Override
-		public SpoutPlayer getPlayer() {
-			return player;
-		}
-
-		@Override
-		public boolean isToken(String token) {
-			return token.equalsIgnoreCase(this.token);
-		}
-
-		@Override
-		public String[] getArgs() {
-			return args.clone();
-		}
-
-		@Override
-		public void setWidget(Plugin p, Widget widget) {
-			this.widget = widget;
-			this.plugin = p;
-		}
-
-		@Override
-		public Widget getWidget() {
-			return widget;
-		}
-
-		@Override
-		public boolean isCancelled() {
-			return cancelled;
-		}
-
-		@Override
-		public void setCancelled(boolean cancel) {
-			cancelled = cancel;
-		}
-
-		@Override
-		public void setIcon(String icon) {
-			this.icon = icon;
 		}
 	}
 }
