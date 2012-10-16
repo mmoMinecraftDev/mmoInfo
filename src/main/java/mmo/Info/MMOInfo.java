2ff702ff1e080000173e20e66092fecc346c97d7
mcsnetworks <mike@mcsnetworks.com>
2012-10-16 17:09:24 -0500

    Implemented the ability to have 4 rows of Tokens Code Cleanup.
    Signed-off-by: mcsnetworks <mike@mcsnetworks.com>

--- src/main/java/mmo/Info/MMOInfo.java
+++ src/main/java/mmo/Info/MMOInfo.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ * This file is part of mmoInfo <http://github.com/mmoMinecraftDev/mmoInfo>.
  *
- * mmoMinecraft is free software: you can redistribute it and/or modify
+ * mmoInfo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -31,6 +31,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
@@ -52,52 +53,76 @@ import org.getspout.spoutapi.gui.WidgetAnchor;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class MMOInfo extends MMOPlugin implements Listener {
-	static String config_info = "{name}~mmoMinecraft~{compass}{coords}";
-	int height = 10; int offset = 1;
+	static String config_info1 = "{name}~mmoMinecraft~{compass}{coords}";
+	static String config_info2 = config_info1;
+	static String config_info3 = config_info1;
+	static String config_info4 = config_info1;
+	static int token_lines = 1;
+	int height = 10;
+	int offset = 1;	
 
 	@Override
 	public EnumBitSet mmoSupport(EnumBitSet support) {
 		support.set(Support.MMO_AUTO_EXTRACT);
 		return support;
 	}
-	
+
 	@Override
-	public void onEnable()
-	{
+	public void onEnable() {
 		super.onEnable();
 		getServer().getPluginManager().registerEvents(this, this);
 	}
+
 	@Override
-	public void loadConfiguration(FileConfiguration cfg)
-	{
-		config_info = cfg.getString("info", config_info);
-	}
-	@EventHandler
-	public void onMMOHUD(MMOHUDEvent event) {
-		Player player = event.getPlayer();
-		if ((player.hasPermission("mmo.info.display")) && (
-				(event.getAnchor() == WidgetAnchor.TOP_LEFT) || (event.getAnchor() == WidgetAnchor.TOP_CENTER) || (event.getAnchor() == WidgetAnchor.TOP_RIGHT)))
-			event.setOffsetY(event.getOffsetY() + this.height + this.offset + 1);
+	public void loadConfiguration(FileConfiguration cfg) {
+		token_lines = cfg.getInt("tokenlines", token_lines);		
+		if (token_lines >= 1) {
+			config_info1 = cfg.getString("info", config_info1);
+		}
+		if (token_lines >= 2) {
+			config_info2 = cfg.getString("info2", config_info2);
+			height = height + 10;
+		}
+		if (token_lines >= 3) {
+			config_info3 = cfg.getString("info3", config_info3);
+			height = height + 10;
+		}
+		if (token_lines >= 4) {
+			config_info4 = cfg.getString("info4", config_info4);
+			height = height + 10;
+		}
 	}
 
 	@EventHandler
-	public void onMMOInfo(MMOInfoEvent event)
-	{
+	public void onMMOHUD(MMOHUDEvent event) {
+		Player player = event.getPlayer();
+		if ((player.hasPermission("mmo.info.display"))
+				&& ((event.getAnchor() == WidgetAnchor.TOP_LEFT)
+						|| (event.getAnchor() == WidgetAnchor.TOP_CENTER) || (event
+								.getAnchor() == WidgetAnchor.TOP_RIGHT)))
+
+			event.setOffsetY(event.getOffsetY() + this.height + this.offset + 1); //
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onMMOInfo(MMOInfoEvent event) {
 		if (event.isToken("name")) {
 			SpoutPlayer player = event.getPlayer();
-			event.setWidget(this.plugin, new GenericLabel(MMO.getName(player)).setResize(true).setFixed(true));
+			event.setWidget(this.plugin, new GenericLabel(MMO.getName(player))
+			.setResize(true).setFixed(true));
 		}
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-	{
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
 		if (command.getName().equalsIgnoreCase("info")) {
-			onSpoutCraftPlayer(SpoutManager.getPlayer((Player)sender));
+			onSpoutCraftPlayer(SpoutManager.getPlayer((Player) sender));
 			return true;
 		}
 		return false;
 	}
+
 	@EventHandler
 	public void onSpoutcraftEnable(SpoutCraftEnableEvent e) {
 		onSpoutCraftPlayer(e.getPlayer());
@@ -108,91 +133,297 @@ public class MMOInfo extends MMOPlugin implements Listener {
 			return;
 		}
 
+		// Global Settings
 		Color back = new Color(0.0F, 0.0F, 0.0F, 0.75F);
 		Color bottom = new Color(1.0F, 1.0F, 1.0F, 0.75F);
 		Screen screen = player.getMainScreen();
-
 		screen.removeWidgets(this);
-		screen.attachWidget(this.plugin, new GenericGradient()
-		.setBottomColor(back)
-		.setTopColor(back)
-		.setMaxWidth(2048)
-		.setX(0)
-		.setY(0)
-		.setWidth(2048)
-		.setHeight(this.height + this.offset)
-		.setAnchor(WidgetAnchor.TOP_LEFT)
-		.setPriority(RenderPriority.Highest));
-		screen.attachWidget(this.plugin, new GenericGradient()
-		.setBottomColor(bottom)
-		.setTopColor(bottom)
-		.setX(0)
-		.setY(this.height + this.offset)
-		.setMaxWidth(2048)
-		.setWidth(2048)
-		.setHeight(1)
-		.setAnchor(WidgetAnchor.TOP_LEFT)
-		.setPriority(RenderPriority.Highest));
-		Container left;
-		screen.attachWidget(this.plugin, 
-				left = (Container)new GenericContainer()
-		.setLayout(ContainerType.HORIZONTAL)
-		.setAlign(WidgetAnchor.TOP_LEFT)
-		.setAnchor(WidgetAnchor.TOP_LEFT)
-		.setWidth(427)
-		.setHeight(this.height)
-		.setX(0)
-		.setY(this.offset));
-		Container center;
-		screen.attachWidget(this.plugin, 
-				center = (Container)new GenericContainer()
-		.setLayout(ContainerType.HORIZONTAL)
-		.setAlign(WidgetAnchor.TOP_CENTER)
-		.setAnchor(WidgetAnchor.TOP_CENTER)
-		.setWidth(427)
-		.setHeight(this.height)
-		.setX(-213)
-		.setY(this.offset));
-		Container right;
-		screen.attachWidget(this.plugin, 
-				right = (Container)new GenericContainer()
-		.setLayout(ContainerType.HORIZONTAL)
-		.setAlign(WidgetAnchor.TOP_RIGHT)
-		.setAnchor(WidgetAnchor.TOP_RIGHT)
-		.setWidth(427)
-		.setHeight(this.height)
-		.setX(-427)
-		.setY(this.offset));
 
-		Container current = left;
-		Matcher match = Pattern.compile("[^{~]+|(~)|\\{([^}]*)\\}").matcher(config_info);
-		while (match.find())
-			if (match.group(1) != null) {
-				if (current == left) {
-					current = center; } else {
-						if (current != center) break;
+		// Global Gradient Image for BlackBox
+		screen.attachWidget(
+				this.plugin,
+				new GenericGradient().setBottomColor(back).setTopColor(back)
+				.setMaxWidth(2048).setX(0).setY(0).setWidth(2048)
+				.setHeight(this.height + this.offset)
+				.setAnchor(WidgetAnchor.TOP_LEFT)
+				.setPriority(RenderPriority.Highest));
+
+		// Global Gradient Image for White Line at bottom of Box
+		screen.attachWidget(
+				this.plugin,
+				new GenericGradient().setBottomColor(bottom)
+				.setTopColor(bottom).setX(0)
+				.setY(this.height + this.offset).setMaxWidth(2048)
+				.setWidth(2048).setHeight(1)
+				.setAnchor(WidgetAnchor.TOP_LEFT)
+				.setPriority(RenderPriority.Highest));
+
+		// Process Token Line 1
+		if (token_lines >= 1) {
+			Container left;
+			screen.attachWidget(
+					this.plugin,
+					left = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_LEFT)
+					.setAnchor(WidgetAnchor.TOP_LEFT).setWidth(427)
+					.setHeight(10).setX(0).setY(this.offset));
+
+			Container center;
+			screen.attachWidget(
+					this.plugin,
+					center = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_CENTER)
+					.setAnchor(WidgetAnchor.TOP_CENTER).setWidth(427)
+					.setHeight(10).setX(-213).setY(this.offset));
+
+			Container right;
+			screen.attachWidget(
+					this.plugin,
+					right = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_RIGHT)
+					.setAnchor(WidgetAnchor.TOP_RIGHT).setWidth(427)
+					.setHeight(10).setX(-427).setY(this.offset));
+
+			Container current = left;
+			Matcher match = Pattern.compile("[^{~]+|(~)|\\{([^}]*)\\}")
+					.matcher(config_info1);
+			while (match.find())
+				if (match.group(1) != null) {
+					if (current == left) {
+						current = center;
+					} else {
+						if (current != center)
+							break;
 						current = right;
 					}
-			}
-			else if (match.group(2) != null) {
-				String[] args = MMO.smartSplit(match.group(2));
-				MMOInfoEventAPI event = new MMOInfoEventAPI(player, args[0], (String[])Arrays.copyOfRange(args, 1, args.length));
-				this.pm.callEvent(event);
-				if ((!event.isCancelled()) && (event.getWidget() != null)) {
-					Widget widget = event.getWidget().setMargin(0, 3);
-					Plugin widgetPlugin = event.getPlugin();
-					if (event.getIcon() != null) {
-						Widget icon = new GenericTexture(event.getIcon()).setMargin(0, 0, 0, 3).setHeight(8).setWidth(8).setFixed(true);
-						screen.attachWidget(widgetPlugin, icon);
-						current.addChild(icon);
+				} else if (match.group(2) != null) {
+					String[] args = MMO.smartSplit(match.group(2));
+					MMOInfoEventAPI event = new MMOInfoEventAPI(player,
+							args[0], (String[]) Arrays.copyOfRange(args, 1,
+									args.length));
+					this.pm.callEvent(event);
+					if ((!event.isCancelled()) && (event.getWidget() != null)) {
+						Widget widget = event.getWidget().setMargin(0, 3);
+						Plugin widgetPlugin = event.getPlugin();
+						if (event.getIcon() != null) {
+							Widget icon = new GenericTexture(event.getIcon())
+							.setMargin(0, 0, 0, 3).setHeight(8)
+							.setWidth(8).setFixed(true);
+							screen.attachWidget(widgetPlugin, icon);
+							current.addChild(icon);
+						}
+						screen.attachWidget(widgetPlugin, widget);
+						current.addChild(widget);
 					}
-					screen.attachWidget(widgetPlugin, widget);
-					current.addChild(widget);
+				} else {
+					String str = match.group().trim();
+					if (!str.isEmpty())
+						current.addChild(new GenericLabel(str).setResize(true)
+								.setMargin(0, 3).setFixed(true));
 				}
-			} else {
-				String str = match.group().trim();
-				if (!str.isEmpty())
-					current.addChild(new GenericLabel(str).setResize(true).setMargin(0, 3).setFixed(true));
-			}
+		}
+
+		// Process Token Line 2
+		if (token_lines >= 2) {
+			Container left2;
+			screen.attachWidget(
+					this.plugin,
+					left2 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_LEFT)
+					.setAnchor(WidgetAnchor.TOP_LEFT).setWidth(427)
+					.setHeight(10).setX(0).setY(this.offset+10));
+
+			Container center2;
+			screen.attachWidget(
+					this.plugin,
+					center2 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_CENTER)
+					.setAnchor(WidgetAnchor.TOP_CENTER).setWidth(427)
+					.setHeight(10).setX(-213).setY(this.offset+10));
+
+			Container right2;
+			screen.attachWidget(
+					this.plugin,
+					right2 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_RIGHT)
+					.setAnchor(WidgetAnchor.TOP_RIGHT).setWidth(427)						
+					.setHeight(10).setX(-427).setY(this.offset+10));
+
+			Container current = left2;
+			Matcher match2 = Pattern.compile("[^{~]+|(~)|\\{([^}]*)\\}").matcher(
+					config_info2);
+			while (match2.find())
+				if (match2.group(1) != null) {
+					if (current == left2) {
+						current = center2;
+					} else {
+						if (current != center2)
+							break;
+						current = right2;
+					}
+				} else if (match2.group(2) != null) {
+					String[] args = MMO.smartSplit(match2.group(2));
+					MMOInfoEventAPI event = new MMOInfoEventAPI(player, args[0],
+							(String[]) Arrays.copyOfRange(args, 1, args.length));
+					this.pm.callEvent(event);
+					if ((!event.isCancelled()) && (event.getWidget() != null)) {
+						Widget widget = event.getWidget().setMargin(0, 3);
+						Plugin widgetPlugin = event.getPlugin();
+						if (event.getIcon() != null) {
+							Widget icon = new GenericTexture(event.getIcon())
+							.setMargin(0, 0, 0, 3).setHeight(8).setWidth(8)
+							.setFixed(true);
+							screen.attachWidget(widgetPlugin, icon);
+							current.addChild(icon);
+						}
+						screen.attachWidget(widgetPlugin, widget);
+						current.addChild(widget);
+					}
+				} else {
+					String str = match2.group().trim();
+					if (!str.isEmpty())
+						current.addChild(new GenericLabel(str).setResize(true)
+								.setMargin(0, 3).setFixed(true));
+				}
+		}
+		// Process Token Line 3
+		if (token_lines >= 3) {
+			Container left3;
+			screen.attachWidget(
+					this.plugin,
+					left3 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_LEFT)
+					.setAnchor(WidgetAnchor.TOP_LEFT).setWidth(427)
+					.setHeight(10).setX(0).setY(this.offset+20));
+
+			Container center3;
+			screen.attachWidget(
+					this.plugin,
+					center3 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_CENTER)
+					.setAnchor(WidgetAnchor.TOP_CENTER).setWidth(427)
+					.setHeight(10).setX(-213).setY(this.offset+20));
+
+			Container right3;
+			screen.attachWidget(
+					this.plugin,
+					right3 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_RIGHT)
+					.setAnchor(WidgetAnchor.TOP_RIGHT).setWidth(427)						
+					.setHeight(10).setX(-427).setY(this.offset+20));
+
+			Container current = left3;
+			Matcher match2 = Pattern.compile("[^{~]+|(~)|\\{([^}]*)\\}").matcher(
+					config_info3);
+			while (match2.find())
+				if (match2.group(1) != null) {
+					if (current == left3) {
+						current = center3;
+					} else {
+						if (current != center3)
+							break;
+						current = right3;
+					}
+				} else if (match2.group(2) != null) {
+					String[] args = MMO.smartSplit(match2.group(2));
+					MMOInfoEventAPI event = new MMOInfoEventAPI(player, args[0],
+							(String[]) Arrays.copyOfRange(args, 1, args.length));
+					this.pm.callEvent(event);
+					if ((!event.isCancelled()) && (event.getWidget() != null)) {
+						Widget widget = event.getWidget().setMargin(0, 3);
+						Plugin widgetPlugin = event.getPlugin();
+						if (event.getIcon() != null) {
+							Widget icon = new GenericTexture(event.getIcon())
+							.setMargin(0, 0, 0, 3).setHeight(8).setWidth(8)
+							.setFixed(true);
+							screen.attachWidget(widgetPlugin, icon);
+							current.addChild(icon);
+						}
+						screen.attachWidget(widgetPlugin, widget);
+						current.addChild(widget);
+					}
+				} else {
+					String str = match2.group().trim();
+					if (!str.isEmpty())
+						current.addChild(new GenericLabel(str).setResize(true)
+								.setMargin(0, 3).setFixed(true));
+				}
+		}
+		//Process Token Line 4
+		if (token_lines >= 4) {
+			Container left4;
+			screen.attachWidget(
+					this.plugin,
+					left4 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_LEFT)
+					.setAnchor(WidgetAnchor.TOP_LEFT).setWidth(427)
+					.setHeight(10).setX(0).setY(this.offset+30));
+
+			Container center4;
+			screen.attachWidget(
+					this.plugin,
+					center4 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_CENTER)
+					.setAnchor(WidgetAnchor.TOP_CENTER).setWidth(427)
+					.setHeight(10).setX(-213).setY(this.offset+30));
+
+			Container right4;
+			screen.attachWidget(
+					this.plugin,
+					right4 = (Container) new GenericContainer()
+					.setLayout(ContainerType.HORIZONTAL)
+					.setAlign(WidgetAnchor.TOP_RIGHT)
+					.setAnchor(WidgetAnchor.TOP_RIGHT).setWidth(427)						
+					.setHeight(10).setX(-427).setY(this.offset+30));
+
+			Container current = left4;
+			Matcher match2 = Pattern.compile("[^{~]+|(~)|\\{([^}]*)\\}").matcher(
+					config_info4);
+			while (match2.find())
+				if (match2.group(1) != null) {
+					if (current == left4) {
+						current = center4;
+					} else {
+						if (current != center4)
+							break;
+						current = right4;
+					}
+				} else if (match2.group(2) != null) {
+					String[] args = MMO.smartSplit(match2.group(2));
+					MMOInfoEventAPI event = new MMOInfoEventAPI(player, args[0],
+							(String[]) Arrays.copyOfRange(args, 1, args.length));
+					this.pm.callEvent(event);
+					if ((!event.isCancelled()) && (event.getWidget() != null)) {
+						Widget widget = event.getWidget().setMargin(0, 3);
+						Plugin widgetPlugin = event.getPlugin();
+						if (event.getIcon() != null) {
+							Widget icon = new GenericTexture(event.getIcon())
+							.setMargin(0, 0, 0, 3).setHeight(8).setWidth(8)
+							.setFixed(true);
+							screen.attachWidget(widgetPlugin, icon);
+							current.addChild(icon);
+						}
+						screen.attachWidget(widgetPlugin, widget);
+						current.addChild(widget);
+					}
+				} else {
+					String str = match2.group().trim();
+					if (!str.isEmpty())
+						current.addChild(new GenericLabel(str).setResize(true)
+								.setMargin(0, 3).setFixed(true));
+				}
+		}
+		
 	}
 }
--- src/main/java/mmo/Info/MMOInfoEventAPI.java
+++ src/main/java/mmo/Info/MMOInfoEventAPI.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ * This file is part of mmoInfo <http://github.com/mmoMinecraftDev/mmoInfo>.
  *
- * mmoMinecraft is free software: you can redistribute it and/or modify
+ * mmoInfo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
